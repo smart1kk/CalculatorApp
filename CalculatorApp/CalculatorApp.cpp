c441a8d38b9a32b35ea9dac868525c26095e9cc6
eeo19 <eeo19@SMART1K>
2022-05-17 20:58:59 +0500

    Добавьте файлы проекта.

---
+++ CalculatorApp/CalculatorApp.cpp
@@ -0,0 +1,28 @@
+﻿// CalculatorApp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to preform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        result = c.Calculate(x, oper, y);
+        cout << "Result is " << result << endl;
+    }
+    return 0;
+}
